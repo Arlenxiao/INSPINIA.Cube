db0d8c2f31cba299a3eb45b541f75399bcaed8f1
arlen <arlen@wslink.cn>
2018-03-01 14:27:44 +0800

    编译版本

--- Cube/INSPINIA.Cube/Properties/AssemblyInfo.cs
+++ Cube/INSPINIA.Cube/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
 [assembly: AssemblyTitle("INSPINIA.Cube")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("新生命魔方INSPINIA皮肤")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("INSPINIA.Cube")]
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // 可以指定所有值，也可以使用“修订号”和“内部版本号”的默认值，
 // 方法是按如下所示使用 "*":
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyFileVersion("1.0.2018.0301")]
